6ab56916c35b5443949622ee4dff2635d0331d00
Sathvik Birudavolu <8377116+BSathvik@users.noreply.github.com>
2020-04-24 21:28:53 -0500

    Rewrite eventCreate

--- app/public/js/event.js
+++ app/public/js/event.js
@@ -2,17 +2,14 @@
 //hook fucntion to submit create event button at end of page
 import { postData, getUrl } from './utility.js';
 
-export function eventAdd(){
+function eventCreate(){
   (async () => {
-      let event = document.getElementById("event").value;
-      let description = document.getElementById("description").value;
-      let dateExample = document.getElementById("dateExample").value;
-      let image = document.getElementById("image").value;
-      let tag1 = document.getElementById("tag1").value;
-      let tag2 = document.getElementById("tag2").value;
-      let tag3 = document.getElementById("tag3").value;
-      let tag4 = document.getElementById("tag4").value;
-      let tag5 = document.getElementById("tag5").value;
+      let event = $("#event").val();
+      let description = $("#description").val();
+      let dateExample = $("#dateExample").val();
+      let image = $("#image").val();
+
+      const tags = Array(5).keys().map(i => $("#tag" + (i + 1)).val())
 
     //jwt  -> tokens cache is browser that user is logged in when making request
       // create this data object
@@ -23,7 +20,7 @@ export function eventAdd(){
         "eventStartTime": dateExample, // Needs to be in-terms of unix time.
         "image": image, 
         "author": "Rahul Alluri",
-        "Tags": [tag1, tag2, tag3, tag4, tag5],
+        "tags": [tag1, tag2, tag3, tag4, tag5],
         "jwt": "<jwt token>"    
     };
     
@@ -34,30 +31,31 @@ export function eventAdd(){
     console.log(JSON.stringify(jsonResponse));
 
     if (jsonResponse["success"] === true) {
-        document.getElementById("output").innerHTML = "Event Successfully posted";
+        $("#output").innerHTML = "Event Successfully posted";
     } else {
         const error = jsonResponse["error"]; // this is the error string;
-        document.getElementById("output").innerHTML = error;
+        $("#output").innerHTML = error;
     }
   
     })();
 }
+
+$("#event-submit").on("click", eventCreate);
 
 export function updateEvent(){
   (async () => {
     //where we will display the corresponding events for user
 
     if (true) {//json author matches user currently using) {
-      document.getElementById("yourevent").innerHTML = "";
+      $("#yourevent").innerHTML = "";
   } else {
       const notFound = jsonResponse["No events found for this user."]; // this is the error string;
-      document.getElementById("yourevent").innerHTML = notFound;
+      $("#yourevent").innerHTML = notFound;
   }
 
 })();
 }
 
-$("#event-submit").on("click", eventAdd);
 $("#load-events").on("click", updateEvent);
 
 function createTagListItem(tagName) {
